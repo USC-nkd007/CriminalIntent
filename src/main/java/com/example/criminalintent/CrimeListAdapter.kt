9eb5f27b454b96a1a72b3d2150092c6301b07a6c
Nicholas Demaio <nkd007@student.usc.edu.au>
2023-09-02 16:56:03 +1000

    Solved icon constraint layout

--- src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -1,10 +1,30 @@
 package com.example.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
+import com.google.android.material.snackbar.Snackbar
 
+class CrimeHolder(
+    val binding: ListItemCrimeBinding
+): RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Snackbar.make(it, "${crime.title} clicked!", Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+}
 class CrimeListAdapter(private val crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
