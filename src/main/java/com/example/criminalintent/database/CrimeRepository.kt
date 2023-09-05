e51a07051ebe5e7bf0c8bf985d75ac9e7555c622
Nicholas Demaio <nkd007@student.usc.edu.au>
2023-09-05 10:58:15 +1000

    Data flows implemented

--- src/main/java/com/example/criminalintent/database/CrimeRepository.kt
+++ src/main/java/com/example/criminalintent/database/CrimeRepository.kt
@@ -3,6 +3,7 @@ package com.example.criminalintent.database
 import android.content.Context
 import androidx.room.Room
 import com.example.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
@@ -18,7 +19,7 @@ class CrimeRepository private constructor(context: Context){
         .createFromAsset(DATABASE_NAME)
         .build()
 
-    suspend fun getCrimes(): List<Crime> = database.crimeDao().getCrimes()
+    fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
 
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
 
